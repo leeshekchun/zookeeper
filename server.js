86b68cf935818b03c6a13c08f6f141bb5c043f5f
ShekChun Lee <leeshekchun@gmail.com>
2022-05-28 12:49:27 -0600

    change port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { animals } = require('./data/animals');
 const express = require('express');
 const { query } = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -53,6 +55,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
